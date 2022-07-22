4e9abbecec4199b0db3560f5efb229ee4a8f51d1
지란지교 <jiransecurity@jiransecurity.onmicrosoft.com>
2022-07-22 17:31:56 +0900

    초기 커밋.

---
+++ CSharp_9_0/4장/ex4_23_delegate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 델리게이트와 object를 이용한 범용 정렬 코드
+namespace CSharp_9_0._4장
+{
+    delegate bool CompareDelegate(object arg1, object arg2);
+
+    class Person
+    {
+        public int Age;
+        public string Name;
+
+        public Person(int age, string name)
+        {
+            this.Age = age;
+            this.Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name + ": " + Age;
+        }
+    }
+    class SortObject
+    {
+        object[] things;
+
+        public SortObject(object[] things)  // object 배열
+        {
+            this.things = things;
+        }
+
+        public void Sort(CompareDelegate compareMethod)
+        {
+            object temp;
+
+            for (int i = 0; i < this.things.Length; i++)
+            {
+                int lowPos = i;
+
+                for (int j = i + 1; j < things.Length; j++)
+                {
+                    if (compareMethod(things[j], things[lowPos]))
+                    {
+                        lowPos = j;
+                    }
+                }
+
+                temp = things[lowPos];
+                things[lowPos] = things[i];
+                things[i] = temp;
+            }
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < things.Length; i++)
+            {
+                Console.WriteLine(things[i] + ",");
+            }
+        }
+    }
+}
---
+++ CSharp_9_0/Program.cs
@@ -0,0 +1,42 @@
+﻿using CSharp_9_0._4장;
+using System;
+
+namespace CSharp_9_0
+{
+    internal class Program
+    {
+        static bool AscSort(object arg1, object arg2)
+        {
+            Person person1 = arg1 as Person;    // 대상 타입으로 형변환
+            Person person2 = arg2 as Person;
+
+            return person1.Age.CompareTo(person2.Age) < 0;
+        }
+
+        static bool DesSort(object arg1, object arg2)
+        {
+            Person person1 = arg1 as Person;    // 대상 타입으로 형변환
+            Person person2 = arg2 as Person;
+
+            return person1.Age.CompareTo(person2.Age) > 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Person[] person = new Person[]
+           {
+                new Person(51, "Anders"),
+                new Person(37, "Scott"),
+                new Person(45, "Peter"),
+                new Person(62, "Mads"),
+           };
+
+            SortObject so = new SortObject(person);
+            so.Sort(AscSort);
+            so.Display();
+            so.Sort(DesSort);
+            so.Display();
+        }
+    }
+}
